62de3d0b7abba4c0d15f6b2fcc3e03d322f03432
SoujanyaChatti <67154494+SoujanyaChatti@users.noreply.github.com>
2020-07-17 17:54:56 +0530

    Add files via upload

---
+++ Box2.js
@@ -0,0 +1,40 @@
+class Box2{
+    constructor(x, y, width, height){
+      var options={
+        'isStatic':false,
+        'restitution':0.3,
+        'friction':0.1
+      }
+    this.body=Bodies.rectangle(x,y,40,40,options);
+    this.width=40;
+    this.height=40;
+    this.Visiblity=255;
+    this.image=loadImage("greensqaure2.jpg");
+    World.add(world,this.body);
+
+    }
+  display(){
+    console.log(this.body.speed);
+    if(this.body.speed<7){
+     imageMode(CENTER);
+     
+      image(this.image,this.body.position.x,this.body.position.y, 40,40);
+    }
+    else{
+   push();
+   World.remove(world,this.body);
+   push();
+    this.Visiblity=this.Visiblity-30;
+   tint(255,this.Visiblity);
+   image(this.image,this.body.position.x,this.body.position.y,40,40);
+    pop();
+    }
+   
+  }
+  
+   
+    
+   
+};
+
+ 
---
+++ Box3.js
@@ -0,0 +1,40 @@
+class Box3{
+    constructor(x, y, width, height){
+      var options={
+        'isStatic':false,
+        'restitution':0.3,
+        'friction':0.1
+      }
+    this.body=Bodies.rectangle(x,y,40,40,options);
+    this.width=40;
+    this.height=40;
+    this.Visiblity=255;
+    this.image=loadImage("redsquare.jpg");
+    World.add(world,this.body);
+
+    }
+  display(){
+    console.log(this.body.speed);
+    if(this.body.speed<15){
+      imageMode(CENTER);
+     
+      image(this.image,this.body.position.x,this.body.position.y, 40,40);
+    }
+    else{
+   push();
+   World.remove(world,this.body);
+   push();
+    this.Visiblity=this.Visiblity-50;
+   tint(255,this.Visiblity);
+   image(this.image,this.body.position.x,this.body.position.y,40,40);
+    pop();
+    }
+   
+  }
+  
+   
+    
+   
+};
+
+ 
